6a0fe554ed1589f080a559dbbb0dd5f5b15bfb23
Lisa Dahl <dahllm@Lisas-iMac.local>
2026-06-29 00:24:38 -0700

    We reverted all that bullshit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
   integrations: [mdx()]
 });
